5e84acfccbf395d74875b203cb4cf7b3e9f571d0
lmach14 <lmach14@freeuni.edu.ge>
2018-02-11 19:37:24 +0400

    networking
    networking

---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class NetworkManager : Photon.PunBehaviour {
+    [SerializeField] private Text status;
+	// Use this for initialization
+	private void Start () {
+        PhotonNetwork.ConnectUsingSettings("1.0");
+	}
+	
+	// Update is called once per frame
+	private void Update () {
+		status.text = PhotonNetwork.connectionStateDetailed.ToString();
+	}
+}
